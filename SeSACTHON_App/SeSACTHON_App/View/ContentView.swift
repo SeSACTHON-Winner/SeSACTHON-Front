854ce6fc2783504a9ded1854da50ec370f0e0888
dayjack <frostjanna12@gmail.com>
2023-06-01 19:55:31 +0900

    [Design/#17] ContentView 디자인 수정

--- SeSACTHON_App/SeSACTHON_App/View/ContentView.swift
+++ SeSACTHON_App/SeSACTHON_App/View/ContentView.swift
@@ -8,23 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isAnythingClicked = false
+    @State var leftTabColor = Color.black
+    @State var rightTabColor = Color.white
+    @State var leftTextColor = Color.white
+    @State var rightTextColor = Color.black
+    @State var isLeftClicked = true
+    
+    
     var body: some View {
         
         NavigationStack {
-            HStack(spacing: 30) {
-                NavigationLink {
-                    MainMapView()
-                } label: {
-                    Text("Map으로 이동")
+            ZStack {
+                HStack(spacing: 0) {
+                    ZStack {
+                        leftTabColor
+                            .ignoresSafeArea()
+                        Text("Map")
+                            .font(.system(size: 75, weight: .heavy))
+                            .foregroundColor(leftTextColor)
+                            .italic()
+                    }
+                    .onTapGesture {
+                        isAnythingClicked = true
+                        leftTabColor = .black
+                        rightTabColor = .white
+                        leftTextColor = .mint
+                        rightTextColor = .black
+                        isLeftClicked = true
+                        UserDefaults().setValue(true, forKey: "isMap")
+                        UserDefaults().setValue(false, forKey: "isRun")
+                        
+                    }
+                    ZStack{
+                        rightTabColor
+                            .ignoresSafeArea()
+                        Text("Run")
+                            .font(.system(size: 75, weight: .heavy))
+                            .foregroundColor(rightTextColor)
+                            .italic()
+                    }
+                    .onTapGesture {
+                        isAnythingClicked = true
+                        leftTabColor = .white
+                        rightTabColor = .black
+                        leftTextColor = .black
+                        rightTextColor = .mint
+                        isLeftClicked = false
+                        UserDefaults().setValue(false, forKey: "isMap")
+                        UserDefaults().setValue(true, forKey: "isRun")
+                    }
                 }
-                NavigationLink {
-                    MainRunView()
-                } label: {
-                    Text("Run으로 이동")
+                VStack {
+                    Spacer()
+                    NavigationLink {
+                        if isLeftClicked {
+                            MainMapView()
+                        } else {
+                            MainRunView()
+                        }
+                    } label: {
+                        Image(isLeftClicked ? "MainGoRight" : "MainGoLeft")
+                            .opacity(isAnythingClicked ? 1 : 0)
+                    }
                 }
-
+                
             }
-            .padding()
+        }
+        .onAppear {
+            if UserDefaults().bool(forKey: "isMap") {
+                isAnythingClicked = true
+                leftTabColor = .black
+                rightTabColor = .white
+                leftTextColor = .mint
+                rightTextColor = .black
+                isLeftClicked = true
+            } else if UserDefaults().bool(forKey: "isRun") {
+                isAnythingClicked = true
+                leftTabColor = .white
+                rightTabColor = .black
+                leftTextColor = .black
+                rightTextColor = .mint
+                isLeftClicked = false
+            }
         }
     }
 }
