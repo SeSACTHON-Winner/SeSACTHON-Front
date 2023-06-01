876439f5411282fe95b6aea4b676d1ca26191026
dayjack <frostjanna12@gmail.com>
2023-06-01 17:49:58 +0900

    [Feat/#13] Background location update 활성화

--- SeSACTHON_App/SeSACTHON_App/Global/Components/CustomMapView.swift
+++ SeSACTHON_App/SeSACTHON_App/Global/Components/CustomMapView.swift
@@ -10,7 +10,10 @@ import MapKit
 
 struct CustomMapView: View {
     
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.5666791, longitude: 126.9782914), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @StateObject var locationDataManager = LocationDataManager()
+    
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: CurrentLocationMO.shared.lat, longitude: CurrentLocationMO.shared.lat), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
 
     
     var body: some View {
