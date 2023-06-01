f209b0f85bef13dfa95e07b80633d93addab460d
dayjack <frostjanna12@gmail.com>
2023-06-01 16:59:47 +0900

    [Design/#9] MainMapView 초안 완성 디자인 완성 시 수정 필요

---
+++ SeSACTHON_App/SeSACTHON_App/Global/Components/MapRouteInfoView.swift
@@ -0,0 +1,63 @@
+//
+//  MapRouteInfoView.swift
+//  SeSACTHON_App
+//
+//  Created by ChoiYujin on 2023/06/01.
+//
+
+import SwiftUI
+
+struct MapRouteInfoView: View {
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            
+            Label("포항시 대이동", systemImage: "smallcircle.filled.circle")
+                .padding(.leading)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 36)
+                .background()
+                .cornerRadius(10)
+            
+            HStack {
+                Spacer()
+                VStack {
+                    Text("7m")
+                        .font(.system(size: 48, weight: .bold))
+                        .foregroundColor(.white)
+                        .italic()
+                    Text("경사도구간")
+                }
+                Spacer()
+                VStack {
+                    Text("3")
+                        .font(.system(size: 48, weight: .bold))
+                        .foregroundColor(.white)
+                        .italic()
+                    Text("장애물")
+                }
+                Spacer()
+                VStack {
+                    Text("15m")
+                        .font(.system(size: 48, weight: .bold))
+                        .foregroundColor(.white)
+                        .italic()
+                    Text("예정시간")
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
+            
+            
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.black)
+    }
+}
+
+struct MapRouteInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapRouteInfoView()
+    }
+}
