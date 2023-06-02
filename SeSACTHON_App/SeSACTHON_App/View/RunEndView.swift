c128ae232033d6537de87df16e528e27414c8c49
radin <dlwlsgml8847@gmail.com>
2023-06-03 01:28:45 +0900

    [Design/#40] 메인컬러 에셋 추가, RunEndView 생성

---
+++ SeSACTHON_App/SeSACTHON_App/View/RunEndView.swift
@@ -0,0 +1,20 @@
+//
+//  RunEndView.swift
+//  SeSACTHON_App
+//
+//  Created by Lee Jinhee on 2023/06/03.
+//
+
+import SwiftUI
+
+struct RunEndView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct RunEndView_Previews: PreviewProvider {
+    static var previews: some View {
+        RunEndView()
+    }
+}
