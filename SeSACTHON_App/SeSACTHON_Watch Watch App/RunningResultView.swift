fbbb145ee3b4ec4d972368bf44cad37b70f44d28
dayjack <frostjanna12@gmail.com>
2023-06-05 13:58:56 +0900

    [Design/#53] RunningEndView 디자인 완료

---
+++ SeSACTHON_App/SeSACTHON_Watch Watch App/RunningResultView.swift
@@ -0,0 +1,20 @@
+//
+//  RunningResultView.swift
+//  SeSACTHON_Watch Watch App
+//
+//  Created by ChoiYujin on 2023/06/05.
+//
+
+import SwiftUI
+
+struct RunningResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RunningResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        RunningResultView()
+    }
+}
