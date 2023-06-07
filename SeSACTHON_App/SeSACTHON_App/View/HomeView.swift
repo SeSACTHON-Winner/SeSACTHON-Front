71d1791ea58126da5e717beb2f4a173dad8ebb58
dayjack <frostjanna12@gmail.com>
2023-06-07 17:55:53 +0900

    [chore/#66] HomeView 뉴스 내용 번경

--- SeSACTHON_App/SeSACTHON_App/View/HomeView.swift
+++ SeSACTHON_App/SeSACTHON_App/View/HomeView.swift
@@ -49,10 +49,10 @@ struct HomeView: View {
                 .shadow(radius: 3, x: 0 ,y: 4)
             ZStack {
                 TabView {
-                    newsView(title: "이달에 대중교통\n임산부석 확대", content: "이달의 교통 뉴스를 확인해보고\n우리가 알릴수 있는 방법을 생각해봐요.", image: Image("Rectangle 136"))
-                    newsView(title: "이달에 대중교통\n임산부석 확대", content: "이달의 교통 뉴스를 확인해보고\n우리가 알릴수 있는 방법을 생각해봐요.", image: Image("Rectangle 136"))
-                    newsView(title: "이달에 대중교통\n임산부석 확대", content: "이달의 교통 뉴스를 확인해보고\n우리가 알릴수 있는 방법을 생각해봐요.", image: Image("Rectangle 136"))
-                    newsView(title: "이달에 대중교통\n임산부석 확대", content: "이달의 교통 뉴스를 확인해보고\n우리가 알릴수 있는 방법을 생각해봐요.", image: Image("Rectangle 136"))
+                    newsView(title: "단차의 높이, 차별의 높이", content: "차도와 보도를 구분하기 위하여 차도에 연석을 설치하는 경우에는 그 높이를 25cm 이하로 해야합니다.이는 외국 도시들(10-15cm)에 비해 높습니다.", image: Image("NewsSample1"))
+                    newsView(title: "교통약자들이 불편해 하는 최소 경사도 8.3%", content: "교통약자의 이동편의 증진법 시행규칙 <별표1>에서 교통약자가 통행할 수 있는 보도의 기울기는 최대 8.3%이하로 하게끔 하고 있습니다.", image: Image("NewsSample2"))
+                    newsView(title: "보도블럭 균열에 넘어지는 교통약자들", content: "교통약자들이 보도블럭의 균열에는 다양한 어려움을 느낄 수 있습니다. 예를 들어, 보행 장애가 있는 사람들은 균열로 인해 휠체어나 보행 보조기구를 움직이는 데에 어려움을 겪을 수 있습니다.", image: Image("NewsSample3"))
+                    newsView(title: "보도의 최소폭은 얼마일까?", content: "보도의 유효폭은 보행자의 통행량과 주변 토지 이용 상황을 고려하여 결정하되, 최소 2미터 이상으로 하여야 합니다.", image: Image("NewsSample4"))
                 }
                 .tabViewStyle(PageTabViewStyle())
             }
@@ -87,73 +87,45 @@ struct HomeView: View {
 extension HomeView {
     
     func newsView(title: String, content: String, image: Image) -> some View {
-            return ZStack {
-                Color.clear.overlay {
-                    image
-                        .resizable()
-                        .scaledToFill()
-                }
-                VStack(alignment: .leading, spacing: 0) {
-                    Spacer()
-                    if isViewMore {
-                        VStack(spacing: 0) {
-                            HStack {
-                               Spacer()
-                                Button {
-                                    isViewMore.toggle()
-                                } label: {
-                                    Image(systemName: "xmark.circle.fill")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 20)
-                                        .foregroundColor(.black)
-                                }
-                                .padding(.vertical)
-                                .padding(.trailing, 18)
-                            }
-                            ScrollView {
-                                Text("기사 내용이들어갈 자리\n기사 내용이들어갈 자리\n기사 내용이들어갈 자리\n")
-                                    .font(.system(size: 18))
-                                    .foregroundColor(.white)
-                                    .fontWeight(.medium)
-                            }
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(.black.opacity(0.4))
-                            .cornerRadius(10)
-                        }
-                    } else {
-                        HStack {
-                            VStack(alignment: .leading, spacing: 10) {
-                                Text("RUN NEWS")
-                                    .font(.system(size: 18))
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.sesacMint)
-                                Text(title)
-                                    .font(.system(size: 24))
-                                    .fontWeight(.heavy)
-                                    .foregroundColor(.white)
-                                Text(content)
-                                    .foregroundColor(.white)
-                                HStack {
-                                    Spacer()
-                                    Button {
-                                        isViewMore.toggle()
-                                    } label: {
-                                        Text("View More +")
-                                            .foregroundColor(.sesacMint)
-                                    }
-                                }
-                                
-                            }
-                            Spacer()
-                        }
-                    }
-                }
-                .padding(.bottom, 78)
-                .padding(.horizontal, 18)
-                .frame(maxWidth: .infinity)
+        return ZStack {
+            Color.clear.overlay {
+                image
+                    .resizable()
+                    .scaledToFill()
             }
+            LinearGradient(
+                colors: [.clear, .black],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            VStack(alignment: .leading, spacing: 0) {
+                Spacer()
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 10) {
+//                        Text("RUN NEWS")
+//                            .font(.system(size: 18))
+//                            .fontWeight(.bold)
+//                            .foregroundColor(.sesacMint)
+                        Text(title)
+                            .font(.system(size: 26))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                            .tracking(1)
+                        Text(content)
+                            .font(.system(size: 18))
+                            .fontWeight(.medium)
+                            .foregroundColor(.white)
+                            .lineSpacing(10)
+                    }
+                    Spacer()
+                }
+            }
+            .padding(.bottom, 78)
+            .padding(.horizontal, 18)
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
