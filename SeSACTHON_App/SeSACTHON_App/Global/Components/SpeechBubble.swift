739cb380ddaef1bae2abe5f54e829f0ab3a5e960
musung <musungkim1352@gmail.com>
2023-06-12 14:43:58 +0900

    [Design/#120] 말풍선 컴포넌트화

---
+++ SeSACTHON_App/SeSACTHON_App/Global/Components/SpeechBubble.swift
@@ -0,0 +1,20 @@
+//
+//  SpeechBubble.swift
+//  SeSACTHON_App
+//
+//  Created by musung on 2023/06/12.
+//
+
+import SwiftUI
+
+struct SpeechBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpeechBubble_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeechBubble()
+    }
+}
