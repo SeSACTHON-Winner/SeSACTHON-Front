708a9ae13d3a21964b5a8f6ca0bc75def3ecef4b
ljh <dusk5588@naver.com>
2023-06-12 17:34:00 +0900

    [Feat/#131] 맵 위험요소 디자인 완성

--- SeSACTHON_App/SeSACTHON_App/Global/Components/PlaceAnnotationView.swift
+++ SeSACTHON_App/SeSACTHON_App/Global/Components/PlaceAnnotationView.swift
@@ -18,7 +18,9 @@ struct PlaceAnnotationView: View {
     var narrowCount: Int
     let listLength: Int
     @State var picturePath: String?
-    
+    @State var dotClick = false
+    var imageMainList = [Image("icon_slope_main"), Image("icon_step_main"), Image("icon_narrow_main"), Image("icon_construction_main")]
+
     init(danger: DangerInfoGroup) {
         self.danger = danger
         self.listLength = danger.list.count
@@ -57,121 +59,142 @@ struct PlaceAnnotationView: View {
         }
     }
     
-    func calWidth() -> Int{
-        var returnValue = 0
-        if slopeCount > 0 {
-            returnValue += 1
+//    func calWidth() -> Int{
+//        var returnValue = 0
+//        if slopeCount > 0 {
+//            returnValue += 1
+//        }
+//        if stepCount > 0 {
+//            returnValue += 1
+//        }
+//        if narrowCount > 0 {
+//            returnValue += 1
+//        }
+//        if constructionCount > 0 {
+//            returnValue += 1
+//        }
+//        print(returnValue)
+//        return returnValue
+//    }
+    
+    func calMain() -> Image{
+        var count = 0
+        var select = 0
+        if slopeCount >=  count {
+            count = slopeCount
+            select = 0
         }
-        if stepCount > 0 {
-            returnValue += 1
+        if stepCount >= count {
+            count = stepCount
+            select = 1
         }
-        if narrowCount > 0 {
-            returnValue += 1
+        if narrowCount >= count {
+            count = narrowCount
+            select = 2
         }
-        if constructionCount > 0 {
-            returnValue += 1
+        if constructionCount >= count {
+            count = constructionCount
+            select = 3
         }
-        print(returnValue)
-        return returnValue
+        
+        return imageMainList[select]
     }
     
     var body: some View {
         VStack(spacing: 0) {
-            KFImage(URL(string: "http://35.72.228.224/sesacthon/\(PicturePath.picturePath)")!)
-                .placeholder { //플레이스 홀더 설정
-                    Image(systemName: "map")
-                }.retry(maxCount: 3, interval: .seconds(5)) //재시도
-                .onSuccess {r in //성공
-                    print("succes: \(r)")
-                }
-                .onFailure { e in //실패
-                    print("failure: \(e)")
-                }
-                .resizable()
-                .scaledToFill()
-                .frame(width: 50, height: 50)
-                .background(.black)
-                .cornerRadius(5)
-            Color.white
-                .frame(width: 50 * CGFloat(calWidth()), height: 80)
-                .cornerRadius(10)
-                .overlay {
-                    HStack {
-                        if slopeCount > 0 {
-                            VStack{
-                                Image("icon_slope")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 32)
-                                Text("\(slopeCount)")
-                                    .foregroundColor(.black)
-                            }
-                        }
-                        if stepCount > 0 {
-                            VStack{
-                                Image("icon_step")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: 32)
-                                Text("\(stepCount)")
-                                    .foregroundColor(.black)
-                            }
-                        }
-                        if constructionCount > 0 {
-                            VStack{
-                                Image("icon_construction")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: 32)
-                                Text("\(constructionCount)")
-                                    .foregroundColor(.black)
-                            }
-                        }
-                        if narrowCount > 0 {
-                            VStack{
-                                Image("icon_narrow")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: 32)
-                                Text("\(narrowCount)")
-                                    .foregroundColor(.black)
-                            }
-                        }
-                    }
-                    //.shadow(radius: 2, x: 2, y: 2)
-                }
             
-            Image(systemName: "arrowtriangle.down.fill")
-                .font(.caption)
-                .scaleEffect(2)
-                .foregroundColor(.white)
-            //                .offset(x: 0, y: 46)
-            //.shadow(radius: 2, x: 0, y: 6)
+            if dotClick {
+                VStack{
+                    Color.white
+                        .frame(width: 200, height: 250)
+                        .cornerRadius(10)
+                        .overlay {
+                            VStack{
+                                KFImage(URL(string: "http://35.72.228.224/sesacthon/\(PicturePath.picturePath)")!)
+                                    .placeholder { //플레이스 홀더 설정
+                                        Image(systemName: "map")
+                                    }.retry(maxCount: 3, interval: .seconds(5)) //재시도
+                                    .onSuccess {r in //성공
+                                        print("succes: \(r)")
+                                    }
+                                    .onFailure { e in //실패
+                                        print("failure: \(e)")
+                                    }
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 150, height: 150)
+                                    .background(.black)
+                                    .cornerRadius(5)
+                                
+                                HStack {
+                                    if slopeCount > 0 {
+                                        VStack{
+                                            Image("icon_slope")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 30)
+                                            Text("\(slopeCount)")
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                    if stepCount > 0 {
+                                        VStack{
+                                            Image("icon_step")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: 30)
+                                            Text("\(stepCount)")
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                    if constructionCount > 0 {
+                                        VStack{
+                                            Image("icon_construction")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: 30)
+                                            Text("\(constructionCount)")
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                    if narrowCount > 0 {
+                                        VStack{
+                                            Image("icon_narrow")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: 30)
+                                            Text("\(narrowCount)")
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    
+                    Image(systemName: "arrowtriangle.down.fill")
+                        .font(.caption)
+                        .scaleEffect(2)
+                        .foregroundColor(.white)
+                }
+                
+            }
             
-            Image("dangerDot")
+            
+            calMain()
                 .resizable()
                 .scaledToFit()
                 .frame(height: 32)
-                .overlay{
-                    Text("\(slopeCount + stepCount + constructionCount + narrowCount)")
-                        .frame(height: 32)
-                        .foregroundColor(.white)
+                .onTapGesture {
+                    dotClick.toggle()
                 }
+                
             
             
         }
-        
-        //.shadow(radius: 2, x: 2, y: 2)
-        
+
     }
 }
 
-
-//struct PlaceAnnotationView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PlaceAnnotationView()
-//    }
-//}
 
 
 class PicturePath {
