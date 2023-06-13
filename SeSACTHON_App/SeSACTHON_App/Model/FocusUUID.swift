845e28d6c9779024d464478e2947c58c2d8313db
ljh <dusk5588@naver.com>
2023-06-13 21:38:40 +0900

    [Feat/#152] 맵뷰 완성

---
+++ SeSACTHON_App/SeSACTHON_App/Model/FocusUUID.swift
@@ -0,0 +1,12 @@
+//
+//  FocusUUID.swift
+//  SeSACTHON_App
+//
+//  Created by 이재혁 on 2023/06/13.
+//
+
+import SwiftUI
+
+class FocusUUID: ObservableObject {
+    @Published var focusUUID: UUID = UUID()
+}
