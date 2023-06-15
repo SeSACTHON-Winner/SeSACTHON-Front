60a7f440a1d859e68c888b743a0ef8c39deb12e0
dayjack <frostjanna12@gmail.com>
2023-06-15 23:13:23 +0900

    [Feat/#203] 워치 신고 화면 타이머 연동 & 결과 화면 타이머 연동

--- SeSACTHON_App/SeSACTHON_Watch Watch App/RunningView.swift
+++ SeSACTHON_App/SeSACTHON_Watch Watch App/RunningView.swift
@@ -18,7 +18,8 @@ struct RunningView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: -10) {
             Spacer()
-            Text("1.2KM")
+            
+            Text("\(String(format: "%.2f", wsManager.watchRunDAO.distance))m")
                 .padding(.top, 30)
                 .padding(.leading, 10)
             RunningTimeView()
